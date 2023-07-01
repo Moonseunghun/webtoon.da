4efba68b5a77543fb4434fbeacf704f799d20453
moonseunghun <mangokkaksae2@gmail.com>
2023-07-01 12:07:47 +0900

    Feat : webtoon by me

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,7 +17,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: const Text(
-          "오늘의 웹툰",
+          "웹툰 의 명작",
           style: TextStyle(
             fontSize: 24,
             fontWeight: FontWeight.w500,
